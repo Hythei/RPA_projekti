29a9b1f54951e2ad85cc53efb6be5b59098048aa
hythei <heikki.hyttinen16@gmail.com>
2024-10-15 11:06:48 +0300

    yes

--- tasks.py
+++ tasks.py
@@ -1,5 +1,14 @@
 from robocorp.tasks import task
 
+# Heikki työskentelyalue
+components = ["AMD Radeon RX 7900XTX", "Nvidia RTX 4060"]
+
+# components arrayta voidaan käsitellä luonnollisesti käyttäen for -looppia tässä tapauksessa, 
+# Voidaan esim käsittelyvaiheess luoda "process_component" -function, joka käy ne läpi yksitellen
+    # def process_component(components):
+        # print(f"Processing component: {component}")
+
+# /Heikki Työskentelyalue
 @task
 def minimal_task():
     message = "Hello"
