81d9aab05332a524236902ca64080c46f02bf582
kaurih <kah00085@laurea.fi>
2024-10-20 19:36:56 +0300

    Lisätty wip funktio, joka kirjoittaa dataa excel-tiedostoon. Tätä täytyy vielä iteroida.

--- tasks.py
+++ tasks.py
@@ -33,7 +33,7 @@ def read_components():
 
 # /Heikki Työskentelyalue
 
-# /Kauri työskentelyalue
+
 
 # /Jasper Työskentelyalue
 components = read_components()
@@ -43,3 +43,65 @@ def minimal_task():
     message = message + " World!"
     print(components)
 
+
+
+
+
+
+
+# /Kauri työskentelyalue
+
+# Lukee (toistaiseksi taulukkoon kovakoodattua) dataa ja
+# tuuttaa sen excel-tiedostoon. Tällä hetkellä aika kämänen ratkaisu,
+# mutta sentään jotain pohjaa. Periaattessa tämän häntäpäähän voisi lisätä toisen toiminnon, joka
+# kopioi komponenttien nimet ja kirjoittaa ne uudelleen A-sarakkeen tyhjiin kohtiin, niin lista uusiutuu.
+def write_to_excel():
+    excel = Files()
+    excel.open_workbook("taulukkotesti.xlsx")
+    
+    # debug-taulukko
+    # tähän sitten joku elegantimpi ratkaisu, kun saadaan revittyä tietoa netistä
+    table = {
+        "price": [600, 700, 350],
+        "shop": ["Verkkokauppa", "Datatronic", "Proshop"]
+    }
+
+    prices = table["price"]
+    shops = table["shop"]
+
+    # ekat tyhjät rivit
+    empty_row = find_next_empty_row(excel)
+
+    # kirjoittaa taulukkoon saatujen rivinumeroiden perusteella
+    for i in range(len(prices)):
+        current_row = empty_row + i
+        excel.set_cell_value(current_row, "B", prices[i])
+        excel.set_cell_value(current_row, "C", shops[i])
+
+    
+    excel.save_workbook()
+
+
+
+# Etsii tyhjiä rivejä rivi nro 2:sta alkaen
+# Tämä EI varmaan ole pomminvarma. Varmaankin hajoaa jos esim. hintatietoa ei löydy
+
+def find_next_empty_row(excel):
+    row = 2 
+    while True:
+        value_in_b = excel.get_cell_value(row, "B") #prices
+        value_in_c = excel.get_cell_value(row, "C")# shops
+        
+        # jos tyhjiä, palauta tämä
+        if not value_in_b and not value_in_c:
+            return row
+        row += 1
+
+
+#def send_email():
+    # funktio, joka etsii dataa excelistä ja lähettää sen sähköpostiin menee tänne
+    # todennäköisesti tarvii robocorp vault -kirjaston sähköpostitilille pääsyyn
+    # get_worksheet_value(row: int, column: Union[str, int], name: Optional[str] = None)
+
+
+# /Kauri työskentelyalue
